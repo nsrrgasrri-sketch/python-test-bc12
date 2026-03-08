59b0b8418daaacbf075f10bae7f847adc6410d24
bur <bur.norrarat@gmail.com>
2026-03-08 05:28:34 +0000

    add new function

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 ## this is my first git and posit push pull
 print("hello world")
 print("I am using github with posit.clound")
+
+def hello():
+  name = input("What is your name: ")
+  print(f"Hello {name}!")
+  
+#test Function
+hello()
